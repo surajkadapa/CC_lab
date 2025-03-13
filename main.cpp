579f446e3563eaf7a82310a895e810cbedaa8296
Suraj <surajkadapa103@gmail.com>
2025-03-13 15:16:15 +0530

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG22CS249 pt2\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
