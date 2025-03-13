da8213d9a4bf3dfa741140ce11945231a541e20f
Suraj <surajkadapa103@gmail.com>
2025-03-13 15:05:35 +0530

    PES2UG22CS249v2

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS249\n";
+  cout << "PES2UG22CS249 pt2\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
